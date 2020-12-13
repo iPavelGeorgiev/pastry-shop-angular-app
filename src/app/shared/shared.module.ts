9d872ce7df34540eebcb68446cfcc6e9dc0d63b8
iPavelGeorgiev <iPavelGeorgiev@outlook.com>
2020-12-13 23:31:11 +0200

    feat: implement carousel component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CarouselComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    CarouselComponent
   ]
 })
 export class SharedModule { }
