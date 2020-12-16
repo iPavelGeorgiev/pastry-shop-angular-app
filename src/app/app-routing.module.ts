494d2d68db59e5d7deb86867fa8d4fec1af31a98
iPavelGeorgiev <iPavelGeorgiev@outlook.com>
2020-12-16 17:36:09 +0200

    feat: add auth module & auth router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToSignIn = () => redirectUnauthorizedTo(["/auth/sign-in"]);
+const redirectLoggedInToHomepage = () => redirectLoggedInTo("/");
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', loadChildren: () => import('./features/homepage/homepage.module').then(m => m.HomepageModule) }
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./features/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
+    ...canActivate(redirectLoggedInToHomepage)
+  }
 ];
 
 @NgModule({
