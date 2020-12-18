3de7a4eaaab16ed1af31f282568a47636de98b51
iPavelGeorgiev <iPavelGeorgiev@outlook.com>
2020-12-18 22:29:30 +0200

    feat: implement icon badge that shows the number of cart items

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +9,28 @@ import { FirebaseService } from '../../services/firebase.service';
 })
 export class HeaderComponent implements OnInit {
   isSignedIn: boolean;
+  cartProducts: number = 0;
 
-  constructor(private firebase: FirebaseService) { }
+  constructor(
+    private firebase: FirebaseService,
+    private db: AngularFirestore
+  ) { }
 
   ngOnInit(): void {
     this.firebase.isSignedIn.subscribe(value => {
       this.isSignedIn = value;
     });
     this.firebase.checkUserStatus();
+
+    let user = localStorage.getItem("user");
+    if (user) user = JSON.parse(user).uid;
+
+    if (user) {
+      const uid = JSON.parse(localStorage.getItem("user")).uid;
+      this.db.collection(`/users/${uid}/cart`).snapshotChanges().subscribe(data => {
+        this.cartProducts = data.length;
+      });
+    }
   }
 
   signOut(): void {
